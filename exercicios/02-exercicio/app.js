7b57a05e9b6c115cdf4fbb2c22240aae0c9ad72c
Vitor Tenente <vitortbrun@outlook.com>
2021-06-02 15:45:40 -0300

    exercicio 2 server e fs

---
+++ exercicios/02-exercicio/app.js
@@ -0,0 +1,40 @@
+
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(
+    (request, response) => {
+
+        let arquivo = '';
+
+        if (request.url === '/') {
+            arquivo = 'index.html';
+        } else if (request.url === '/hobbies.html') {
+            arquivo = 'hobbies.html'
+        } else if (request.url === '/contato.html') {
+            arquivo = 'contato.html'
+        } else {
+            arquivo = 'erro.html'
+        }
+
+        if (fs.existsSync(arquivo)) {
+            fs.readFile(arquivo, 
+                (erro, dados) => {
+                    if (erro) {
+                        throw erro;
+                    } else {
+                        response.writeHead(200 /*status sucesso*/, {'Content-Type': 'text/html'});
+                        response.end(dados);
+                    }
+                }
+            );    
+        } else {
+            response.writeHead(500, {'Content-Type': 'text/html'});
+            response.write(`Arquivo ${arquivo} não localizado...`);
+            response.end();
+        }
+    }
+);
+
+server.listen(3000, ()=> {console.log("Servidor está rodando, escutando a porta 3000")});
